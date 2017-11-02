d35407bf2fb4f4afc03d68024d623c82e4a278e5
Dima Kudosh <LagMegantic@yandex.ru>
2017-11-02 21:06:35 +0300

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+
+
+setup(
+    name='django-prepared-query',
+    version='0.1',
+    packages=['django_prepared_query'],
+    url='https://github.com/DimaKudosh/django-prepared-query',
+    license='MIT',
+    author='Dima Kudosh',
+    author_email='dimakudosh@gmail.com',
+    description='Prepared statements support for Django',
+    keywords=['django'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Developers',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+    ],
+)
