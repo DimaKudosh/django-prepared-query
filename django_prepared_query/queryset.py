ffd1b5c19e142cdf156051f7121e9928135a4e78
Dima Kudosh <LagMegantic@yandex.ru>
2017-11-09 21:55:25 +0300

    Refactor queryset class

--- django_prepared_query/queryset.py
+++ django_prepared_query/queryset.py
@@ -28,37 +28,35 @@ class PrepareQuerySet(QuerySet):
         return super(PrepareQuerySet, self).__repr__()
 
     def __iter__(self):
-        if self.prepared:
-            raise OperationOnPreparedStatement('PreparedQuerySet object is not iterable')
+        self._check_prepared('PreparedQuerySet object is not iterable')
         return self._base_iter()
 
     def __len__(self):
-        if self.prepared:
-            raise OperationOnPreparedStatement('PreparedQuerySet object has no len()')
+        self._check_prepared('PreparedQuerySet object has no len()')
         return super(PrepareQuerySet, self).__len__()
 
     def __bool__(self):
-        if self.prepared:
-            raise OperationOnPreparedStatement('PreparedQuerySet can\'t be fetched without calling execute method')
+        self._check_prepared('PreparedQuerySet can\'t be fetched without calling execute method')
         return super(PrepareQuerySet, self).__bool__()
 
     def __getitem__(self, item):
-        if self.prepared:
-            raise OperationOnPreparedStatement('PreparedQuerySet can\'t be fetched without calling execute method')
+        self._check_prepared('PreparedQuerySet can\'t be fetched without calling execute method')
         return super(PrepareQuerySet, self).__getitem__(item)
 
     def __and__(self, other):
-        if self.prepared:
-            raise OperationOnPreparedStatement('AND not allowed on prepared statement')
+        self._check_prepared('AND not allowed on prepared statement')
         return super(PrepareQuerySet, self).__and__(other)
 
     def __or__(self, other):
-        if self.prepared:
-            raise OperationOnPreparedStatement('OR not allowed on prepared statement')
+        self._check_prepared('OR not allowed on prepared statement')
         return super(PrepareQuerySet, self).__or__(other)
 
     def _base_iter(self):
         return super(PrepareQuerySet, self).__iter__()
+
+    def _check_prepared(self, msg):
+        if self.prepared:
+            raise OperationOnPreparedStatement(msg)
 
     def _generate_prepare_statement_name(self):
         return '%s_%s' % (self.model._meta.model_name, generate_random_string(self.HASH_LENGTH))
@@ -125,167 +123,134 @@ class PrepareQuerySet(QuerySet):
         return list(qs._base_iter())
 
     def iterator(self):
-        if self.prepared:
-            raise OperationOnPreparedStatement('Iterator not allowed on prepared statement')
+        self._check_prepared('Iterator not allowed on prepared statement')
         return super(PrepareQuerySet, self).iterator()
 
     def aggregate(self, *args, **kwargs):
-        if self.prepared:
-            raise OperationOnPreparedStatement('Aggregate not allowed on prepared statement')
+        self._check_prepared('Aggregate not allowed on prepared statement')
         return super(PrepareQuerySet, self).aggregate(*args, **kwargs)
 
     def count(self):
-        if self.prepared:
-            raise OperationOnPreparedStatement('Count not allowed on prepared statement')
+        self._check_prepared('Count not allowed on prepared statement')
         return super(PrepareQuerySet, self).count()
 
     def get(self, *args, **kwargs):
-        if self.prepared:
-            raise OperationOnPreparedStatement('Get not allowed on prepared statement')
+        self._check_prepared('Get not allowed on prepared statement')
         return super(PrepareQuerySet, self).get(*args, **kwargs)
 
     def create(self, **kwargs):
-        if self.prepared:
-            raise OperationOnPreparedStatement('Create not allowed on prepared statement')
+        self._check_prepared('Create not allowed on prepared statement')
         return super(PrepareQuerySet, self).create(**kwargs)
 
     def bulk_create(self, objs, batch_size=None):
-        if self.prepared:
-            raise OperationOnPreparedStatement('Bulk Create not allowed on prepared statement')
+        self._check_prepared('Bulk Create not allowed on prepared statement')
         return super(PrepareQuerySet, self).bulk_create(objs, batch_size)
 
     def get_or_create(self, defaults=None, **kwargs):
-        if self.prepared:
-            raise OperationOnPreparedStatement('Get or create not allowed on prepared statement')
+        self._check_prepared('Get or create not allowed on prepared statement')
         return super(PrepareQuerySet, self).get_or_create(defaults=defaults, **kwargs)
 
     def update_or_create(self, defaults=None, **kwargs):
-        if self.prepared:
-            raise OperationOnPreparedStatement('Update or create not allowed on prepared statement')
+        self._check_prepared('Update or create not allowed on prepared statement')
         return super(PrepareQuerySet, self).update_or_create(defaults=defaults, **kwargs)
 
     def _earliest_or_latest(self, field_name=None, direction="-"):
-        if self.prepared:
-            raise OperationOnPreparedStatement('Earliest or latest not allowed on prepared statement')
+        self._check_prepared('Earliest or latest not allowed on prepared statement')
         return super(PrepareQuerySet, self)._earliest_or_latest(field_name=field_name, direction=direction)
 
     def first(self):
-        if self.prepared:
-            raise OperationOnPreparedStatement('First not allowed on prepared statement')
+        self._check_prepared('First not allowed on prepared statement')
         return super(PrepareQuerySet, self).first()
 
     def last(self):
-        if self.prepared:
-            raise OperationOnPreparedStatement('Last not allowed on prepared statement')
+        self._check_prepared('Last not allowed on prepared statement')
         return super(PrepareQuerySet, self).last()
 
     def in_bulk(self, id_list=None):
-        if self.prepared:
-            raise OperationOnPreparedStatement('Operation not allowed on prepared statement')
+        self._check_prepared('Operation not allowed on prepared statement')
         return super(PrepareQuerySet, self).in_bulk(id_list=id_list)
 
     def delete(self):
-        if self.prepared:
-            raise OperationOnPreparedStatement('Delete not allowed on prepared statement')
+        self._check_prepared('Delete not allowed on prepared statement')
         return super(PrepareQuerySet, self).delete()
 
     def update(self, **kwargs):
-        if self.prepared:
-            raise OperationOnPreparedStatement('Update not allowed on prepared statement')
+        self._check_prepared('Update not allowed on prepared statement')
         return super(PrepareQuerySet, self).update(**kwargs)
 
     def exists(self):
-        if self.prepared:
-            raise OperationOnPreparedStatement('Exists not allowed on prepared statement')
+        self._check_prepared('Exists not allowed on prepared statement')
         return super(PrepareQuerySet, self).exists()
 
     def raw(self, raw_query, params=None, translations=None, using=None):
-        if self.prepared:
-            raise OperationOnPreparedStatement('Raw not allowed on prepared statement')
+        self._check_prepared('Raw not allowed on prepared statement')
         return super(PrepareQuerySet, self).raw(raw_query, params=params, translations=translations, using=using)
 
     def values(self, *fields, **expressions):
-        if self.prepared:
-            raise OperationOnPreparedStatement('Values not allowed on prepared statement')
+        self._check_prepared('Values not allowed on prepared statement')
         return super(PrepareQuerySet, self).values(*fields, **expressions)
 
     def values_list(self, *fields, **kwargs):
-        if self.prepared:
-            raise OperationOnPreparedStatement('Values list not allowed on prepared statement')
+        self._check_prepared('Values list not allowed on prepared statement')
         return super(PrepareQuerySet, self).values_list(*fields, **kwargs)
 
     def dates(self, field_name, kind, order='ASC'):
-        if self.prepared:
-            raise OperationOnPreparedStatement('Dates not allowed on prepared statement')
+        self._check_prepared('Dates not allowed on prepared statement')
         return super(PrepareQuerySet, self).dates(field_name, kind, order)
 
     def datetimes(self, field_name, kind, order='ASC', tzinfo=None):
-        if self.prepared:
-            raise OperationOnPreparedStatement('Datetimes not allowed on prepared statement')
+        self._check_prepared('Datetimes not allowed on prepared statement')
         return super(PrepareQuerySet, self).datetimes(field_name, kind, order, tzinfo)
 
     def none(self):
-        if self.prepared:
-            raise OperationOnPreparedStatement('None not allowed on prepared statement')
+        self._check_prepared('None not allowed on prepared statement')
         return super(PrepareQuerySet, self).none()
 
     def all(self):
-        if self.prepared:
-            raise OperationOnPreparedStatement('All not allowed on prepared statement')
+        self._check_prepared('All not allowed on prepared statement')
         return super(PrepareQuerySet, self).all()
 
     def _filter_or_exclude(self, negate, *args, **kwargs):
-        if self.prepared:
-            raise OperationOnPreparedStatement('Filter not allowed on prepared statement')
+        self._check_prepared('Filter not allowed on prepared statement')
         return super(PrepareQuerySet, self)._filter_or_exclude(negate, *args, **kwargs)
 
     def select_related(self, *fields):
-        if self.prepared:
-            raise OperationOnPreparedStatement('Select related not allowed on prepared statement')
+        self._check_prepared('Select related not allowed on prepared statement')
         return super(PrepareQuerySet, self).select_related(*fields)
 
     def prefetch_related(self, *lookups):
-        if self.prepared:
-            raise OperationOnPreparedStatement('Prefetch related not allowed on prepared statement')
+        self._check_prepared('Prefetch related not allowed on prepared statement')
         return super(PrepareQuerySet, self).prefetch_related(*lookups)
 
     def annotate(self, *args, **kwargs):
-        if self.prepared:
-            raise OperationOnPreparedStatement('Annotate not allowed on prepared statement')
+        self._check_prepared('Annotate not allowed on prepared statement')
         return super(PrepareQuerySet, self).annotate(*args, **kwargs)
 
     def order_by(self, *field_names):
-        if self.prepared:
-            raise OperationOnPreparedStatement('Order by not allowed on prepared statement')
+        self._check_prepared('Order by not allowed on prepared statement')
         return super(PrepareQuerySet, self).order_by(*field_names)
 
     def distinct(self, *field_names):
-        if self.prepared:
-            raise OperationOnPreparedStatement('Distinct not allowed on prepared statement')
+        self._check_prepared('Distinct not allowed on prepared statement')
         return super(PrepareQuerySet, self).distinct(*field_names)
 
     def extra(self, select=None, where=None, params=None, tables=None,
               order_by=None, select_params=None):
-        if self.prepared:
-            raise OperationOnPreparedStatement('Extra not allowed on prepared statement')
+        self._check_prepared('Extra not allowed on prepared statement')
         return super(PrepareQuerySet, self).extra(select, where, params, tables, order_by, select_params)
 
     def reverse(self):
-        if self.prepared:
-            raise OperationOnPreparedStatement('Reverse not allowed on prepare statement')
+        self._check_prepared('Reverse not allowed on prepare statement')
         return super(PrepareQuerySet, self).reverse()
 
     def defer(self, *fields):
-        if self.prepared:
-            raise OperationOnPreparedStatement('Defer not allowed on prepare statement')
+        self._check_prepared('Defer not allowed on prepare statement')
         return super(PrepareQuerySet, self).defer(*fields)
 
     def only(self, *fields):
-        if self.prepared:
-            raise OperationOnPreparedStatement('Only not allowed on prepare statement')
+        self._check_prepared('Only not allowed on prepare statement')
         return super(PrepareQuerySet, self).only(*fields)
 
     def using(self, alias):
-        if self.prepared:
-            raise OperationOnPreparedStatement('Using not allowed on prepare statement')
+        self._check_prepared('Using not allowed on prepare statement')
         return super(PrepareQuerySet, self).using(alias)
