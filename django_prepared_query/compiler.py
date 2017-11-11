35fdf928fcee014ab3ceaded76338b456be23ec3
Dima Kudosh <LagMegantic@yandex.ru>
2017-11-11 23:25:47 +0300

    Added MySQL support

--- django_prepared_query/compiler.py
+++ django_prepared_query/compiler.py
@@ -1,5 +1,5 @@
 from django.db.models.sql.compiler import SQLCompiler
-from django.db.models.sql.constants import CURSOR
+from django.db.models.sql.constants import CURSOR, MULTI
 from django.db.models import AutoField, BigAutoField, IntegerField, BigIntegerField
 from .operations import PreparedOperationsFactory
 
@@ -39,14 +39,39 @@ class PrepareSQLCompiler(SQLCompiler):
 
 
 class ExecutePrepareSQLCompiler(SQLCompiler):
+    def __init__(self, query, connection, using):
+        super(ExecutePrepareSQLCompiler, self).__init__(query, connection, using)
+        self.params_order = self.query.prepare_params_order
+        prepare_params_values = self.query.prepare_params_values
+        params = []
+        for param_name in self.params_order:
+            params.append(prepare_params_values[param_name])
+        self.params = params
+        self.prepared_operations = PreparedOperationsFactory.create(self.connection.vendor)
+
     def as_sql(self, with_limits=True, with_col_aliases=False):
         self.pre_sql_setup()
-        prepare_params_values = self.query.prepare_params_values
-        params_order = self.query.prepare_params_order
-        params = []
-        for param_name in params_order:
-            params.append(prepare_params_values[param_name])
-        prepared_operations = PreparedOperationsFactory.create(self.connection.vendor)
-        execute_statement = prepared_operations.execute_sql(name=self.query.prepare_statement_name,
-                                                            arguments=params_order)
+        execute_statement = self.prepared_operations.execute_sql(name=self.query.prepare_statement_name,
+                                                                 arguments=self.params_order)
+        params = self.params if self.params and not self.prepared_operations.has_setup() else ()
         return execute_statement, params
+
+    def setup_execute_sql(self):
+        if not self.prepared_operations.has_setup():
+            return
+        setup_sql = self.prepared_operations.setup_execute_sql(self.params_order)
+        if not setup_sql:
+            return
+        cursor = self.connection.cursor()
+        try:
+            cursor.execute(setup_sql, self.params)
+        except Exception as original_exception:
+            try:
+                cursor.close()
+            except Exception:
+                pass
+            raise original_exception
+
+    def execute_sql(self, result_type=MULTI, chunked_fetch=False):
+        self.setup_execute_sql()
+        return super(ExecutePrepareSQLCompiler, self).execute_sql(result_type, chunked_fetch)
--- django_prepared_query/operations.py
+++ django_prepared_query/operations.py
@@ -8,6 +8,13 @@ class PreparedOperations:
     def execute_sql(self, name, arguments):
         raise NotImplementedError
 
+    def setup_execute_sql(self, arguments):
+        raise NotImplementedError
+
+    @staticmethod
+    def has_setup():
+        raise NotImplementedError
+
     def prepare_placeholder(self, index):
         raise NotImplementedError
 
@@ -25,12 +32,44 @@ class PostgresqlPreparedOperations(PreparedOperations):
             arguments_sql = '(%s)' % ','.join('%s' for _ in range(len(arguments)))
         return 'EXECUTE %s%s;' % (name, arguments_sql)
 
+    @staticmethod
+    def has_setup():
+        return False
+
+    def setup_execute_sql(self, arguments):
+        return None
+
     def prepare_placeholder(self, index):
         return '$%d' % index
 
 
 class MySqlPreparedOperations(PreparedOperations):
-    pass
+    VARIABLE_TEMPLATE = '@var%d'
+
+    def prepare_sql(self, name, arguments, sql):
+        return "PREPARE %s FROM \"%s\";" % (name, sql.replace('\\', '\\\\'))
+
+    def execute_sql(self, name, arguments):
+        execute_sql = 'EXECUTE %s' % name
+        if arguments:
+            variables = [self.VARIABLE_TEMPLATE % i for i in range(len(arguments))]
+            arguments_sql = ','.join(name for name in variables)
+            execute_sql = '%s USING %s' % (execute_sql, arguments_sql)
+        return execute_sql + ';'
+
+    @staticmethod
+    def has_setup():
+        return True
+
+    def setup_execute_sql(self, arguments):
+        if not arguments:
+            return None
+        variables = [self.VARIABLE_TEMPLATE % i for i in range(len(arguments))]
+        sql = 'SET %s;' % ','.join(['{} = %s'.format(name) for name in variables])
+        return sql
+
+    def prepare_placeholder(self, index):
+        return '?'
 
 
 class OraclePreparedOperations(PreparedOperations):
